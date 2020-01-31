890f59fa7116351e213199e401d4ed3715c8cd33
toorevitimirp <1345569965@gmail.com>
2020-01-31 14:49:22 +0800

    测试

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,15 +2,14 @@ from flask import Flask
 from flask_cors import cross_origin
 
 app = Flask(__name__)
-#日志
+# 日志
 from api.logger import log
 
-#注册路由
+# 注册路由
 import api.route
 
-#错误处理
+# 错误处理
 import api.error
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
