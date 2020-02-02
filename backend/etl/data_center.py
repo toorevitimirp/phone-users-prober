d6204b66ad8758749beabcb48dafaf12de441da9
toorevitimirp <1345569965@gmail.com>
2020-02-02 16:47:37 +0800

    增加删除数据的api‘

--- backend/etl/data_center.py
+++ backend/etl/data_center.py
@@ -1,21 +1,23 @@
 import pandas as pd
-from flask import Blueprint,request
+import json
+from flask import Blueprint, request
 from flask_cors import cross_origin
-from etl.database import save_file,get_collection_info
+from etl.database import save_file, get_collection_info, del_data_by_collection_name
 from etl.washer import wash_data
 
-data_center = Blueprint('data_center', __name__,url_prefix='/data-center/api/v1.0')
+data_center = Blueprint('data_center', __name__, url_prefix='/data-center/api/v1.0')
 
-@data_center.route('/data',methods=['POST'])
+
+@data_center.route('/data', methods=['POST'])
 @cross_origin()
 def post_data():
     if request.method == 'POST':
         collection = request.values['collection']
         if collection == 'data-info':
-            return {'result':500,'msg':'上传失败，数据集合名称不能为data-info'}
+            return {'result': 500, 'msg': '上传失败，数据集合名称不能为data-info'}
 
-        user_data = pd.read_csv(request.files["data_all"],encoding='utf-8')
-        complain_users = pd.read_csv(request.files["data_label"],encoding='utf-8')["user_id"]
+        user_data = pd.read_csv(request.files["data_all"], encoding='utf-8')
+        complain_users = pd.read_csv(request.files["data_label"], encoding='utf-8')["user_id"]
         all_users_id = user_data["user_id"]
         labels = all_users_id.isin(complain_users).astype("int")
 
@@ -23,27 +25,38 @@ def post_data():
         clean_data = wash_data(user_data)
         try:
             length = clean_data.shape[0]
-            res = save_file(collection,clean_data.to_json(orient='records'),length)
+            res = save_file(collection, clean_data.to_json(orient='records'), length)
         except BaseException as e:
-            print("exception",e)
-            res = {'result':500,'msg':'上传失败，未知错误'}
+            print("exception", e)
+            res = {'result': 500, 'msg': '上传失败，未知错误'}
         finally:
             return res
         # save_file(collection,user_data)
-        
+
         # clean_data = wash_data(user_data)
-        
+
         # descriptions = describe(clean_data)
         # return jsonify(descriptions)
+
 
 @data_center.route('/data/info')
 @cross_origin()
 def get_data_info():
     try:
         collection_info = get_collection_info()
-        res = {'result':0,'data':collection_info}
+        res = {'result': 0, 'data': collection_info}
     except BaseException as e:
-        print('exception:',e)
-        res = {'result':500,'data':None}
+        print('exception:', e)
+        res = {'result': 500, 'data': None}
     finally:
         return res
+
+
+@data_center.route('/data/del', methods=['POST'])
+@cross_origin()
+def del_data():
+    if request.method == 'POST':
+        data = request.get_data()
+        json_data = json.loads(data.decode('utf-8'))
+        name = json_data['name']
+    return del_data_by_collection_name(name)
--- backend/etl/database.py
+++ backend/etl/database.py
@@ -1,9 +1,11 @@
-import pymongo,json,time,os
-from  api.logger import log
+import pymongo, json, time, os
+from api.logger import log
 from pandas import DataFrame
 
-data_info = "data-info" #储存集合元数据
-def _connect_mongo(host, port, username=None, password=None,db='phone-number-prober'):
+data_info = "data-info"  # 储存集合元数据
+
+
+def _connect_mongo(host, port, username=None, password=None, db='phone-number-prober'):
     """ A util for making a connection to mongo """
     if username and password:
         mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
@@ -11,17 +13,16 @@ def _connect_mongo(host, port, username=None, password=None,db='phone-number-pro
     else:
         conn = pymongo.MongoClient(host, port)
     return conn[db]
-# client = pymongo.MongoClient('mongodb://localhost:27017/')
-# db = client['phone-number-prober']
-# print(collist)
-def save_file(collection,user_data,length):
+
+
+def save_file(collection, user_data, length):
     start = time.time()
-    db = _connect_mongo(host='localhost',port=27017)
+    db = _connect_mongo(host='localhost', port=27017)
 
     collist = db.list_collection_names()
     if collection in collist:
-        return {'result':500,'msg':'上传失败名称已存在'}
-    
+        return {'result': 500, 'msg': '上传失败名称已存在'}
+
     # try:
     #     db[collection].insert_many(json.loads(user_data))
     # except BaseException as e:
@@ -32,30 +33,56 @@ def save_file(collection,user_data,length):
     try:
         db[collection].insert_many(json.loads(user_data))
     except BaseException as e:
-        log("exception"+str(e))
-        res = {'result':500,'msg':'上传失败'}
+        log("exception" + str(e))
+        res = {'result': 500, 'msg': '上传失败'}
         return res
     else:
         try:
-            info = {'name':collection, 'length':length, 'trained':0}
+            info = {'name': collection, 'length': length, 'trained': 0}
             db[data_info].insert_one(info)
         except BaseException as e:
-            log("exception"+str(e))
+            log("exception" + str(e))
             db[collection].drop()
-            res = {'result':500,'msg':'上传失败'}
+            res = {'result': 500, 'msg': '上传失败'}
             return res
         else:
             end = time.time()
-            log("上传文件到数据库成功，花费时间："+str(end-start))
-    
-    return {'result':200,'msg':'上传成功'}
+            log("上传文件到数据库成功，花费时间：" + str(end - start))
+
+    return {'result': 200, 'msg': '上传成功'}
+
 
 def load_data(collection):
-    db = _connect_mongo(host='localhost',port=27017)
+    db = _connect_mongo(host='localhost', port=27017)
     user_data = DataFrame(list(db[collection].find()))
     return user_data
 
+
 def get_collection_info():
-    db = _connect_mongo(host='localhost',port=27017)
-    res =  list(db[data_info].find({},{ "_id": 0,}))
+    db = _connect_mongo(host='localhost', port=27017)
+    res = list(db[data_info].find({}, {"_id": 0, }))
     return res
+
+
+def del_data_by_collection_name(name):
+    # 应该先删除元数据
+
+    try:
+        db = _connect_mongo(host='localhost', port=27017)
+        query = {'name': name}
+        temp = list(db[data_info].find(query))[0]
+        db[data_info].delete_one(query)
+    except BaseException as e:
+        print('exception:', e)
+        res = {'result': 500, 'msg': '删除数据失败'}
+    else:
+        res = {'result': 200, 'msg': '删除数据成功'}
+        try:
+            db[name].drop()
+            res = {'result': 200, 'msg': '删除数据成功'}
+        except BaseException as e:
+            print('exception', e)
+            db[data_info].insert_one(temp)
+            res = {'result': 500, 'msg': '删除数据失败'}
+    finally:
+        return res
